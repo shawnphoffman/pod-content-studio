aeaaf6e4c7296d4fd50c8d17cf654449251fb00e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-28 10:20:08 -0400

    Improvements

--- app/(pages)/faq/page.tsx
+++ app/(pages)/faq/page.tsx
@@ -1,12 +1,28 @@
-import { Suspense } from 'react'
-
-import Awards from '@/components/Awards'
+import Link from 'next/link'
 
 export default async function Page() {
 	return (
-		<>
-			<h1>WTF is this???</h1>
-			<p>Wow cool</p>
-		</>
+		<article className="prose dark:prose-invert lg:prose-xl prose-zinc">
+			<h1>What is this?</h1>
+			<p>
+				There are two main goals for this site. One is to allow people to create and edit custom content for their websites, and two is to
+				make all of this easy and self-service.
+			</p>
+			<p>
+				Currently, you&apos;re in the preview section of the site. This area is your reassurance, displaying all the content across various
+				podcast sites and allowing you to get a basic preview before it goes live. To create or edit content, simply click on the
+				<Link href="/studio" className="mx-1 hover:text-sky-500">
+					Studio Editor
+				</Link>{' '}
+				option in the navigation bar, guiding you to where you need to be.
+			</p>
+			<h2>Warning</h2>
+			<p>
+				Please do not modify other podcasts&apos; content. I do not have permissions set up yet because they&apos;re a pain, and I&apos;d
+				like to avoid them. If this becomes an issue, I can always add them to make things a bit safer. Please try not to do anything dumb.
+			</p>
+			<h2>Finally</h2>
+			<p>If you have questions or see something wrong, you know where to find me.</p>
+		</article>
 	)
 }
--- components/podcasts/PodcastRow.tsx
+++ components/podcasts/PodcastRow.tsx
@@ -1,23 +1,32 @@
-// import PostAuthor from '../posts/PostAuthor'
-
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { urlFor } from '@/lib/sanity/sanity.image'
+import { PODCASTS_QUERYResult } from '@/lib/sanity/sanity.types'
 
-export default function PodcastRow({ podcast }) {
+type Props = {
+	podcast: PODCASTS_QUERYResult[number]
+}
+
+export default function PodcastRow({ podcast }: Props) {
 	return (
-		<div className="flex flex-col items-center gap-2 p-4 py-2 transition-all rounded-lg hover:dark:bg-sky-950/50 hover:bg-sky-100/50 md:flex-row md:gap-4">
+		<div className="flex flex-col items-center gap-1 p-4 py-2 transition-all rounded-lg hover:dark:bg-sky-950/50 hover:bg-sky-100/50 md:flex-row md:gap-4">
 			{podcast.image && (
 				<Image
-					className="h-auto w-96 md:w-24"
+					className="h-auto w-full md:w-24 max-md:max-w-48"
 					width={200}
 					height={200}
-					alt={podcast.image?.alt || ''}
+					alt={podcast.title || ''}
 					src={urlFor(podcast.image).height(200).width(200).url()}
 					sizes="100vw"
 				/>
 			)}
-			<h3 className="p-4 text-lg font-medium">{podcast.title}</h3>
+			<div className="py-2 text-lg font-medium flex-1">{podcast.title}</div>
+			{podcast.webUrl && (
+				<Link className="hover:text-sky-500" href={podcast.webUrl}>
+					{podcast.webUrl.replace('https://', '').replace('/', '')}
+				</Link>
+			)}
 		</div>
 	)
 }
