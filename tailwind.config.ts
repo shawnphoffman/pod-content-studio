56a20733fb575fe7221409914896deaa4e731c0a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-04 14:38:54 -0400

    Add tw to studio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,13 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
 	content: [
 		//
-		'./sanity/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./lib/**/*.{js,ts,jsx,tsx,mdx}',
 		// Flowbite
 		'./node_modules/flowbite/**/*.js',
 	],
 	theme: {},
-
 	darkMode: 'media',
 	plugins: [
 		//
