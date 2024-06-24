2ac7127faffb66610c706b7e2222166a9ccde14c
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-24 08:23:35 -0700

    fix youtube embeds

--- components/portableText/YoutubeEmbed.tsx
+++ components/portableText/YoutubeEmbed.tsx
@@ -37,7 +37,6 @@ const LazyYoutube = ({ videoId }: LazyYoutubeProps) => {
 					height="400"
 					src={`https://www.youtube.com/embed/${videoId}`}
 					title="YouTube video player"
-					frameBorder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 					allowFullScreen
 				></iframe>
